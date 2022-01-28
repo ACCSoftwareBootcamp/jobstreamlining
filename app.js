e498800a99230927d6139be34c22e625ef2e8a20
emily-02 <59155619+emily-02@users.noreply.github.com>
2022-01-28 08:05:18 -0600

    adding header, footer, and css page

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => {
-  res.render("home.ejs");
-});
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`App on port ${port}`));
